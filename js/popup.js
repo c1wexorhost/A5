8870344bf98b5e5b5d191a105e7eaa3fbebbd900
abdulrashid <abdulrashidkalodi@gmail.com>
2024-04-08 13:54:24 +0530

    button,timedealy popup functionality added

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
   "use strict";
 
-  // Function to show the newsletter popup
-  function showNewsletterPopup() {
+  var nameInput = document.getElementById("name");
+  var emailInput = document.getElementById("email");
+  var telInput = document.getElementById("tel");
+
+  // Function to show a popup
+  function showPopup(popupId) {
     $.magnificPopup.open({
       items: {
-        src: "#newsletter-popup-form",
+        src: popupId,
       },
       type: "inline",
       removalDelay: 350,
@@ -21,73 +25,42 @@ $(document).ready(function () {
       },
     });
   }
-
   // Show newsletter popup after 5 seconds
   setTimeout(function () {
     var mpInstance = $.magnificPopup.instance;
     if (mpInstance.isOpen) {
       mpInstance.close();
     }
-
-    showNewsletterPopup();
-  }, 3000); // 5000 milliseconds = 5 seconds
-  // Function to show the offer popup
-  function showOfferPopup() {
-    $.magnificPopup.open({
-      items: {
-        src: "#offer-popup",
-      },
-      type: "inline",
-      removalDelay: 350,
-      callbacks: {
-        open: function () {
-          $("body").css("overflow-x", "visible");
-          $(".sticky-header.fixed").css("padding-right", "1.7rem");
-        },
-        close: function () {
-          $("body").css("overflow-x", "hidden");
-          $(".sticky-header.fixed").css("padding-right", "0");
-        },
-      },
-    });
-  }
+    showPopup("#newsletter-popup-form");
+  }, 3000);
 
   // Event listener for clicking on the offer tag
   $(document).on("click", "#offer_tag", function (e) {
     e.preventDefault(); // Prevent the default action of the link
-    showOfferPopup(); // Show the offer popup
+    showPopup("#offer-popup"); // Show the offer popup
   });
-
   // Form submission handling
   document
     .getElementById("contactForm")
     .addEventListener("submit", function (event) {
       event.preventDefault(); // Prevent form submission
 
-      // Get form values
-      var name = document.getElementById("name").value.trim();
-      var email = document.getElementById("email").value.trim();
-      var tel = document.getElementById("tel").value.trim();
+      var name = nameInput.value.trim();
+      var email = emailInput.value.trim();
+      var tel = telInput.value.trim();
       var client_id = "wexorai"; // Replace with your user ID
-
-      // Check if name, email, and tel fields are not empty
       if (name === "" || email === "" || tel === "") {
         alert("Name, Email, and Mobile Number are required fields.");
-        return; // Exit the function if any of the required fields are empty
+        return;
       }
-
-      // Duplicate tel value into message
       var message = `Tel: ${tel}\n\n Message:`;
-
-      // Send JSON request using Fetch API
       var payload = {
         client_id: client_id,
         name: name,
         email: email,
         tel: tel,
-        message: message, // Only tel is appended to the message
+        message: message,
       };
-
       fetch("https://chatbot.wexorai.com/clientmessagef3", {
         method: "POST",
         headers: {
@@ -101,10 +74,10 @@ $(document).ready(function () {
             alert(
               "Offer message sent successfully! Our team will connect with you shortly."
             );
-            gtag('event', 'conversion', { 'send_to': 'AW-11467448022/8iw9CM7iv6QZENa9jdwq' });
             // Reset form fields except name, email, and tel
-            document.getElementById("subject").value = "";
-            document.getElementById("message").value = "";
+            nameInput.value = "";
+            emailInput.value = "";
+            telInput.value = "";
           } else {
             throw new Error("Failed to send message.");
           }
@@ -112,9 +85,53 @@ $(document).ready(function () {
         .catch(function (error) {
           alert("An error occurred: " + error.message);
         });
+    });
+  // Form submission handling for the button click popup
+  document
+    .getElementById("contactFormButton")
+    .addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent form submission
 
-      // Reset form fields except name, email, and tel
-      document.getElementById("subject").value = "";
-      document.getElementById("message").value = "";
+      var name = document.getElementById("nameButton").value.trim();
+      var email = document.getElementById("emailButton").value.trim();
+      var tel = document.getElementById("telButton").value.trim();
+      var client_id = "wexorai"; // Replace with your user ID
+
+      if (name === "" || email === "" || tel === "") {
+        alert("Name, Email, and Mobile Number are required fields.");
+        return;
+      }
+      var message = `Tel: ${tel}\n\n Message:`;
+      var payload = {
+        client_id: client_id,
+        name: name,
+        email: email,
+        tel: tel,
+        message: message,
+      };
+      fetch("https://chatbot.wexorai.com/clientmessagef3", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+        redirect: "follow",
+      })
+        .then(function (response) {
+          if (response.ok) {
+            alert(
+              "Offer message sent successfully! Our team will connect with you shortly."
+            );
+            // Reset form fields except name, email, and tel
+            document.getElementById("nameButton").value = "";
+            document.getElementById("emailButton").value = "";
+            document.getElementById("telButton").value = "";
+          } else {
+            throw new Error("Failed to send message.");
+          }
+        })
+        .catch(function (error) {
+          alert("An error occurred: " + error.message);
+        });
     });
 });
